0736e69cec71e808d9ffac6c8790963d3f70505f
KimJaeGeun <jgk9612@gmail.com>
2022-05-06 21:27:51 +0900

    feat: 220506 Django Study
    Django Study

--- myapp/views.py
+++ myapp/views.py
@@ -1,11 +1,60 @@
 from django.shortcuts import render, HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+#import random
 
-# Create your views here.
-def index( request ) :
-    return HttpResponse('Welcome!')
+topics = [
+    {'id':1, 'title':'routing', 'body':'Routing is ..'},
+    {'id':2, 'title':'view', 'body':'View is ..'},
+    {'id':3, 'title':'Model', 'body':'Model is ..'},
+]
 
-def create(request) :
-    return HttpResponse('Create!')
+def HTMLTemplate(articleTag):
+    global topics
+    ol = ''
+    for topic in topics:
+        ol += f'<li><a href="/read/{topic["id"]}">{topic["title"]}</a></li>'
+    return f'''
+    <html>
+    <body>
+        <h1><a href="/">Django</a></h1>
+        <ul>
+            {ol}
+        </ul>
+        {articleTag}
+        <ul>
+            <li><a href="/create/">create</a></li>
+        </ul>
+    </body>
+    </html>
+    '''
+
+def index(request) :
+    article = '''
+    <h2>Welcome</h2>
+    Hello, Django
+    '''
+    return HttpResponse(HTMLTemplate(article))
 
 def read(request, id) :
-    return HttpResponse('Read!' + id)
+    global topics
+    article = ''
+    for topic in topics :
+        if topic['id'] == int(id) :
+            article = f'<h2>{topic["title"]}</h2>{topic["body"]}'
+    return HttpResponse(HTMLTemplate(article))
+
+@csrf_exempt
+def create(request) :
+    if request.method == 'GET' :
+        article = '''
+            <form action="/create/" method="post">
+                <p><input type="text" name="title" placeholder="title"></p>
+                <p><textarea name="body" placeholder="body"></textarea></p>
+                <p><input type="submit"></p>
+            </form>
+        '''
+        return HttpResponse(HTMLTemplate(article))
+    elif request.method == 'POST' :
+        title = request.POST['title']
+        body = request.POST['body']
+        return HttpResponse(request.POST['title'])
